518a5e13b4a187cfe8353ef875288b251e39ee80
ATarsa <smb909@gmail.com>
2018-10-24 20:16:32 +0100

    change colours to dark-purple and deep-yellow

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+/* Function written with referencing w3schools.com:
+
+https://www.w3schools.com/howto/howto_js_toggle_class.asp 
+accessesd on 24/10/2018*/
+
 function displayMenu() {
   let menuList = document.getElementById('menu');
 
